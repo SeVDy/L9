f24e8bbf2f1d367a72e7ef8c82b32c78d957f357
flap <savemoree@gmail.com>
2023-11-27 21:12:24 +0300

    Изменены тесты

--- testInPytest.py
+++ testInPytest.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
 class TestCard:
     def setup(self):
         self.card = Card()
+        self.card1 = Card()
 
     def teardown(self):
         pass
@@ -29,6 +30,13 @@ class TestCard:
             assert num_str == 4 and num_int == 5, f'Некорректно заполняется строка карты №{j}!'
             assert len(set(self.card.getCardInfo[i])) == 6, f'В строке №{i} есть дубликаты!'
 
+    def test__str__(self):
+        print()
+        assert self.card.__str__() == '', 'функция Str работает некорректно!'
+
+    def test__eq__(self):
+        assert self.card != self.card1, 'функция eq работает некорректно!'
+
     def test_modify_card(self):
         flag = False
         for i in range(1, 91):
@@ -46,13 +54,25 @@ class TestCard:
 class TestGenerator:
     def setup(self):
         self._x = [x for x in range(1, 91)]
-        self.generator = Generator()
+        self.generator, self.generator1 = Generator()
 
     def teardown(self):
         pass
 
     def test_init(self):
         assert self.generator.poolList == self._x, 'Некорректно создается список с бочонками!'
+
+    def test__int__(self):
+        assert int(self.generator) == 0, 'Некорректно работает метод int!'
+
+    def test__str__(self):
+        assert str(self.generator) == '0', 'Некорректно работает метод str!'
+
+    def test__eq__(self):
+        assert self.generator == self.generator1, 'Некорректно работает метод eq!'
+
+    def test__len__(self):
+        assert len(self.generator) == 90, 'Некорректно работает метод len!'
 
     def test_mix_bag(self):
         temp_list = []
@@ -64,13 +84,19 @@ class TestGenerator:
 
 class TestPlayer:
     def setup(self):
-        self.player = Player()
+        self.player, self.player1 = Player()
         self.maskWin = [[], [], []]
         for i in range(3):
             for j in range(4):
                 self.maskWin[i].append('  ')
             for k in range(5):
                 self.maskWin[i].append('--')
+
+    def test__str__(self):
+        assert str(self.player) == 'Player', 'Некорректно работает метод str!'
+
+    def test__eq__(self):
+        assert self.player != self.player1, 'Некорректно работает метод eq!'
 
     def test_win(self):
         assert self.player.win(self.maskWin), 'Неверно определяется признак победителя!'
@@ -84,6 +110,9 @@ class TestNPC:
 
     def teardown(self):
         pass
+
+    def test__str__(self):
+        assert str(self.NPC) == 'NPC', 'Некорректно работает метод str!'
 
     def test_get_answer(self):
         answ_mask = 'нет'
